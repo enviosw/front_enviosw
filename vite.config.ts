39ceeca41495b02945ffcef098b6e329122f4623
millerbermeo <millerbermeo30@gmail.com>
2025-05-03 19:38:18 -0500

    commit fornssasasa

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
   server: {
-    host: '0.0.0.0',  // Permite que Vite sea accesible desde cualquier dispositivo en tu red
-    port: 3000,  // El puerto en el que el servidor de desarrollo estará escuchando
-    strictPort: true,  // Fuerza el uso del puerto definido, sin intentar puertos alternativos
-    open: true,  // Abre automáticamente el navegador en el servidor de desarrollo
+    host: '0.0.0.0',  // Para ser accesible desde cualquier dispositivo de la red
+    port: 5173, // El puerto en desarrollo
   },
   build: {
-    outDir: 'dist',  // Directorio de salida para los archivos de producción
-    sourcemap: false,  // Desactiva los mapas de fuentes en producción (opcional)
-    minify: 'terser',  // Usa Terser para minificar el código en producción
+    outDir: 'dist',  // Directorio de salida
+    sourcemap: false,
+    minify: 'terser',
   },
-  base: './',  // Asegura que la base de los recursos sea relativa
+  base: './',  // Es importante para desplegar correctamente en producción
 });
