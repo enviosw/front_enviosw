95ca205a2cd854d05c2931121e8eb65abd50adb4
WilfrenDeveloper <wilfrenmoney@gmail.com>
2025-05-11 18:48:12 -0500

    advances

--- src/shared/layout/AdminLayout.tsx
+++ src/shared/layout/AdminLayout.tsx
@@ -9,13 +9,13 @@ const AdminLayout: React.FC<Contenido> = ({ children }) => {
         <SidebarProvider>
             <div className="flex max-w-full h-dvh relative bg-[#eeeeee]">
                 <Sidebar />
-                <div className="flex-1 overflow-y-auto h-[95vh] p-10 mt-[5vh]  bg-[#FDFDFD] border-1 border-gray-300 rounded-tl-3xl">
+                {/* <div className="flex-1 overflow-y-auto h-[95vh] p-10 mt-[5vh]  bg-[#FDFDFD] border-1 border-gray-300 rounded-tl-3xl"> */}
+                <div className="w-full bg-white p-6 overflow-y-auto rounded-lg m-2">
                     {/* <Navbar /> */}
                     {children}
                 </div>
             </div>
         </SidebarProvider>
-
 
     );
 };
