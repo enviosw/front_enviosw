23c922e2718b810a71e5b4cbe5e9bf43a7782589
WilfrenDeveloper <wilfrenmoney@gmail.com>
2025-05-11 19:04:27 -0500

    correción en el van del dashboard

--- src/context/SidebarContext.tsx
+++ src/context/SidebarContext.tsx
@@ -20,19 +20,11 @@ export const SidebarProvider: React.FC<Contenido> = ({ children }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     useEffect(() => {
-        // Tailwind's sm breakpoint = 640px
-        const handleResize = () => {
             if (window.innerWidth > 640) {
                 setIsOpen(true);
+            }else{
+                setIsOpen(false);
             }
-        };
-
-        // Llama al inicio
-        handleResize();
-
-        // También escucha cambios de tamaño
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
     }, []);
 
     const toggleSidebar = () => setIsOpen(prev => !prev);
