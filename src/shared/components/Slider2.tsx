a8deea6ec33d94bb055c5e192471e099213d7b9e
millerbermeo <millerbermeo30@gmail.com>
2025-05-13 00:07:08 -0500

    commit ajustes

--- src/shared/components/Slider2.tsx
+++ src/shared/components/Slider2.tsx
@@ -152,7 +152,7 @@ const Slider2: React.FC = () => {
 
 
     return (
-        <div className="w-full overflow-hidden relative mb-8">
+        <div className="w-full overflow-hidden relative mb-8 h-[35vh] lg:h-[42vh]">
             {/* Contenedor deslizante */}
             <div
                 className="flex transition-transform duration-1000 ease-in-out"
