1f321d3b6dfb947d0cb05dcb0ae964b70ad1e970
millerbermeo <millerbermeo30@gmail.com>
2025-06-04 20:25:09 -0500

    commit

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -2,15 +2,31 @@ import axios from 'axios';
 import { BASE_URL } from './baseUrl';
 
 const useAxiosInstance = () => {
-    const token = localStorage.getItem('access_token'); // ✅ toma el token del localStorage
+    const token = localStorage.getItem('access_token');
 
     const axiosInstance = axios.create({
         baseURL: BASE_URL,
         headers: {
             'Content-Type': 'application/json',
-            ...(token ? { Authorization: `Bearer ${token}` } : {}), // ✅ agrega el header si existe
+            ...(token ? { Authorization: `Bearer ${token}` } : {}),
         },
     });
+
+    // ✅ Interceptor para respuestas con error
+    axiosInstance.interceptors.response.use(
+        (response) => response,
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                // 🔴 Eliminar token
+                localStorage.removeItem('access_token');
+
+                // 🔴 Redirigir al login o recargar
+                window.location.href = '/login'; // Cambia a tu ruta de login real
+            }
+
+            return Promise.reject(error);
+        }
+    );
 
     return axiosInstance;
 };
