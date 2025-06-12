6d0136307455c61acf9ba0279d19dc21cd41e5c2
millerbermeo <millerbermeo30@gmail.com>
2025-06-11 21:09:34 -0500

    commit validacion

--- src/utils/Loader.tsx
+++ src/utils/Loader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader: React.FC = () => {
     return (
         <div className="fixed inset-0 z-50 bg-transparent flex items-center justify-center pointer-events-none">
-            <img src="loader.gif" alt="Cargando..." className="size-32" />
+            <img src="/loader.gif" alt="Cargando..." className="size-32" />
         </div>
     );
 };
